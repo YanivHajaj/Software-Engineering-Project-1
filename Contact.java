0e2567de27ee4f5e1d4d49c252494604fcf2fe43
samy-ne <110774495+samy-ne@users.noreply.github.com>
2023-05-21 17:17:40 +0300

    updated_main
    updated main

--- Contact.java
+++ Contact.java
@@ -1,11 +1,22 @@
+package pack1;
+//GROUP 13: SAMY, SHAIEL, YANIV, ILYA, ORI, TOMER
 public class Contact{
 	private String name;
 	private String number;
 	
 	public void SetName(String name){this.name=name;}
-	public void SetNumber(String number){this.number=number;}
+	public void SetNumber(String number){
+		try {
+		    int check = Integer.parseInt(number);
+		    this.number=number;
+		} catch (NumberFormatException e) {
+			System.out.println("An invalid number: '" + number + "' was entered. We set the number to 0000000000.");
+			this.number="0000000000";
+		}
+	}	
 	public String GetName(){return name;}
 	public String GetNumber(){return number;};
+	public Contact Get(){return this;};
 	public void PrintContact(){System.out.println(name + ": " + number);}
 
 }
--- Phonebook.java
+++ Phonebook.java
@@ -1,93 +1,277 @@
+package pack1;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner; //to get input from user
+//GROUP 13: SAMY, SHAIEL, YANIV, ILYA, ORI, TOMER
 
-//include Contact;
 public class Phonebook {
 	
-	// print all contacts in the phonebook
-	public static void printAllContact(ArrayList<Contact> phonebook) {
-		System.out.println("~~~all contacts in the phone book: ~~~");
-		for (Contact contact : phonebook) {
-			contact.PrintContact();
+	//easier to check (similar to 1 but add from main)
+	public static void addContactFromMain(ArrayList<Contact> phonebook, String name, String number) {
+	    Contact newContact = new Contact();
+	    newContact.SetName(name);
+	    newContact.SetNumber(number);
+	    phonebook.add(newContact);
+	}
+	
+	//1)
+	public static void addContactFromUser(ArrayList<Contact> phonebook) {
+	    
+	    System.out.print("Enter name of the contact: \n");
+	    String name = input.nextLine();
+	     System.out.print("Enter phone number of the contact: \n");
+	    String number = input.nextLine();
+	    Contact newContact = new Contact();
+	    newContact.SetName(name);
+	    newContact.SetNumber(number);	    
+	    phonebook.add(newContact);
+	}
+	
+	
+	//2)
+	public static void DeleteContact(ArrayList<Contact> phonebook) {
+	  System.out.print("Enter name of the contact you wnat to delte: ");
+	  String Name = input.nextLine();
+	  int index=0;
+	  try {
+      for (Contact cont : phonebook) {
+          if (cont.GetName().compareTo(Name)==0) {
+              break;
+          }
+          index=index+1;
+      }
+      phonebook.remove(index);
+	  }
+	  catch (IndexOutOfBoundsException e) {
+		  System.out.println("We weren't able to remove the contact because it wasn't on your phoneook.");
+	  }
+  }
+	
+	// 3) print all contacts in the phonebook
+		public static void printAllContact(ArrayList<Contact> phonebook) {
+			System.out.println("~~~all contacts in the phone book: ~~~");
+			for (Contact contact : phonebook) {
+				contact.PrintContact();
+			}
+			System.out.println("~~~all contacts in the phone book done ~~~");
 		}
-		System.out.println("~~~all contacts in the phone book: ~~~");
-	}
-	public static void DeleteContact(ArrayList<Contact> phonebook,String Name) {
-        int index=0;
-        for (Contact cont : phonebook) {
-            if (cont.GetName().compareTo(Name)==0) {
-                break;
-            }
-            index=index+1;
-        }
-        phonebook.remove(index);
-    }
 	
-	public static void FindContact(ArrayList<Contact> phonebook,String Name) {
-		System.out.println("~~~contact with name: "+ Name+" in the phonebook: ~~~"); 
-		for (Contact cont : phonebook) {
-	            if (cont.GetName().equals(Name)) {
-	            	cont.PrintContact();
-	            }
-	        }
-		 System.out.println("~~~contact inside phonebook with name: "+ Name+" : ~~~");
-	}
-	
-	public static void deleteDuplicateNames(ArrayList<Contact> phonebook) {
-	    ArrayList<String> uniqueNames = new ArrayList<String>();
-	    ArrayList<Contact> contactsToDelete = new ArrayList<Contact>();
-	    for (Contact cont : phonebook) {
-	        if (uniqueNames.contains(cont.GetName())) {
-	            contactsToDelete.add(cont);
-	        } else { 
-	            uniqueNames.add(cont.GetName());
+	//4)
+	public static ArrayList<Contact> findNameOccurrences(ArrayList<Contact> phonebook) {
+	    ArrayList<Contact> AllOccurrences = new ArrayList<Contact>();
+	    System.out.print("Enter a name to search:  ");
+	    String name = input.nextLine();  
+	    for (Contact contact : phonebook) {
+	        if (contact.GetName().equals(name)) {
+	        	 AllOccurrences.add(contact);
+	        	 contact.PrintContact(); //we weren't sure if you wanted to return simply the occurences or the information to the user
+	        	 //if we wants only to return and not print to the user we can simply take out the contact.PrintContact(); line
 	        }
 	    }
-	    for (Contact cont : contactsToDelete) {
-	        DeleteContact(phonebook,cont.GetName());
-	    }
+	    return AllOccurrences;
 	}
 	
-    
+	
+	//delete duplicates (7)
+	//if there is more than one copy leave only one
+	public static void deleteDuplicatenew(ArrayList<Contact> phonebook) {
+			for (int i = 0; i<phonebook.size(); i++) {//overall n loops
+				for (int j=0; j<phonebook.size(); j++) { //in each loop going n checks
+					if (i!=j) {
+						if (phonebook.get(j).GetName().compareTo(phonebook.get(i).GetName()) == 0) {//and we have the same name
+							if (phonebook.get(j).GetNumber().compareTo(phonebook.get(i).GetNumber()) == 0) {//and we have the same number
+									phonebook.remove(i);
+									
+								}
+							}
+						}
+					}
+				}	
+			}
+
+
+		
+	
+	
+	static Scanner input = new Scanner(System.in); //Create a Scanner object
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		ArrayList<Contact> phonebook = new ArrayList<Contact>();
-
-		// add some contacts to the phonebook
-		Contact contact1 = new Contact();
-		contact1.SetName("moahe");
-		contact1.SetNumber("111-1234");
-		phonebook.add(contact1);
+		 
+		boolean exit = false;
 		
-		Contact contact2 = new Contact();
-		contact2.SetName("samy");
-		contact2.SetNumber("222-5678");
-		phonebook.add(contact2);
-				
-		Contact contact3 = new Contact();
-		contact3.SetName("avraham");
-		contact3.SetNumber("333-1234");
-		phonebook.add(contact3);
 		
-		Contact contact4 = new Contact();
-		contact4.SetName("shlomo");
-		contact4.SetNumber("444-1111");
-		phonebook.add(contact4);
+		// add some contacts to the phonebook (can be deleted)
+		addContactFromMain(phonebook, "moahe", "1111234");
+		addContactFromMain(phonebook, "david", "2225678");
+		addContactFromMain(phonebook, "shlomo", "4443333");
+		addContactFromMain(phonebook, "avraham", "3331234");
+		addContactFromMain(phonebook, "shlomo", "4441111");
+		addContactFromMain(phonebook, "shlomo", "4442222");
+		addContactFromMain(phonebook, "shlomo", "4443333");
+		addContactFromMain(phonebook, "shlomo", "4443333"); 
+		addContactFromMain(phonebook, "shlomo", "4443333");
 		
-		Contact contact5 = new Contact();
-		contact5.SetName("shlomo");
-		contact5.SetNumber("444-2222");
-		phonebook.add(contact5);
-		
-		Contact contact6 = new Contact();
-		contact6.SetName("shlomo");
-		contact6.SetNumber("444-3333");
-		phonebook.add(contact6);
-		
-		String cont2Name=contact2.GetName();
-		DeleteContact(phonebook,cont2Name); //delete moshe
-		FindContact(phonebook,"shlomo");
-		deleteDuplicateNames(phonebook);
-		printAllContact(phonebook);
-
+		while (!exit) {
+			System.out.println("Type the number of one of the following instructions to excute it:");
+			System.out.println("1) Add contact");
+			System.out.println("2) Delete contact");
+			System.out.println("3) Display all contacts");
+			System.out.println("4) Search contact by name (return ArrayList that contains the occurrences)");
+			System.out.println("5) Sort all contacts by name");
+			System.out.println("6) Sort all contacts by number");
+			System.out.println("7) Delete duplicates");
+			System.out.println("8) Reverse the order of the phonebook");
+			System.out.println("9) Save phonebook as a text file");
+			System.out.println("10) Load contacts from a text file");
+			System.out.println("11) Exit the program");
+			String Num = input.next();
+			switch (Num) {
+			case "1":
+				addContactFromUser(phonebook);
+				break;
+			case "2":	
+				DeleteContact(phonebook);
+				break;
+			case "3":
+				printAllContact(phonebook);
+				break;
+			case "4":
+				findNameOccurrences(phonebook);	
+				break;
+			case "5":
+				sortListByName(phonebook);
+				break;
+			case "6":
+				sortListByNumber(phonebook);
+				break;
+			case "7":
+				deleteDuplicatenew(phonebook);
+				break;
+			case "8":
+				reverseOrder(phonebook);
+				System.out.println("The order was reversed.");
+				break;
+			case "9":
+				saveToTextFile(phonebook);
+				break;
+			case "10":
+				addFromFile(phonebook);
+				break;
+			case "11":
+				System.out.println("Exiting. We will not take a new input");
+				exit= true;
+				break;
+			default:
+				System.out.println("Unavialable");
+				break;
+			}
+		}
 	}
 	
+	//reverse the order (8)
+	public static void reverseOrder(ArrayList<Contact> phonebook) {   
+        if (phonebook == null ) {
+        	System.out.println("Couldn't reverse the order - there are no contacts in list"); 
+        	return;
+        }
+        else if ( phonebook.size() <= 1){
+        	return;
+        }
+        else {
+        Contact personInList = phonebook.remove(0); //remove the first contact in the list
+        reverseOrder(phonebook);// call the function again
+        phonebook.add(personInList);//add the removed elements in reverse order
+        }
+	}
+	
+	
+//sort 5 (by name from small to big)
+	public static void sortListByName(ArrayList<Contact> phonebook) {
+		int n = phonebook.size();
+		Contact temp;
+		//using bubble sort
+		for (int i = 0; i<n; i++) {//overall n loops
+			for (int j=1; j<(n-i); j++) { //in each loop going n-i-1 checks
+				 if (phonebook.get(j-1).GetName().compareTo(phonebook.get(j).GetName()) > 0) {
+					 //if the element to the left is bigger than the element to the right switch them
+					 temp = phonebook.get(j-1);
+					 phonebook.set((j-1),phonebook.get(j));
+					 phonebook.set(j,temp);
+				 }
+			}
+		}
+	}
+	
+	//sort 6 (by number from big to small)
+	public static void sortListByNumber(ArrayList<Contact> phonebook) {
+		int n = phonebook.size();
+		Contact temp;
+		for (int i = 0; i<n; i++) {
+			for (int j=1; j<(n-i); j++) {
+				 if (phonebook.get(j).GetNumber().compareTo(phonebook.get(j-1).GetNumber()) > 0) {
+					 temp = phonebook.get(j-1);
+					 phonebook.set((j-1),phonebook.get(j));
+					 phonebook.set(j,temp);
+				 }
+			}
+		}
+	}
+	
+	//nine 
+	public static void saveToTextFile(ArrayList<Contact> phonebook) {
+			
+			String filename ;
+			System.out.println("Enter file name for all contacts to be saves: ");
+			filename = input.next();
+			filename = filename + ".txt";
+			
+			try {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+				for (int i = 0; i < phonebook.size(); i++) {
+					writer.write(phonebook.get(i).GetName()+ ": " + phonebook.get(i).GetNumber()+"\n");
+				}
+				
+				//writer.write("writing to file");
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			
+		}
+	
+	
+	
+	//ten
+	public static void addFromFile(ArrayList<Contact> phonebook) {
+			String filename;
+			System.out.println("Enter file name for contacts to be loaded from: ");
+			filename = input.next();
+			filename = filename +".txt";
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(filename));
+				String line;
+				Contact bufferContact;
+				
+				while((line = reader.readLine())!=null) {
+					//read a contact and insert to a new contact instance
+					bufferContact = new Contact();
+					String[] data = line.split(": "); 
+					bufferContact.SetName(data[0]);
+					bufferContact.SetNumber(data[1]);
+					//now the buffercontact is set and ready to be entered to the arraylist
+					phonebook.add(bufferContact);
+					
+				}
+				
+				reader.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
 }
