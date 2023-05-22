ba7ee9166e8bae6ea656c51d96322c45e46ed5c6
samy-ne <110774495+samy-ne@users.noreply.github.com>
2023-05-22 18:02:25 +0300

    refactor + main sms

--- Contact.java
+++ Contact.java
@@ -1,22 +1,33 @@
 //package pack1;
 //GROUP 13: SAMY, SHAIEL, YANIV, ILYA, ORI, TOMER
 public class Contact{
-	private String name;
-	private String number;
+	private String _name;
+	private String _number;
 	
-	public void SetName(String name){this.name=name;}
+	
+	
+	
+	Contact(String name, String number)
+	{
+		this._name = name;
+		this._number = number;
+	}
+	
+	
+	
+	public void SetName(String name){this._name=name;}
 	public void SetNumber(String number){
 		try {
 		    int check = Integer.parseInt(number);
-		    this.number=number;
+		    this._number=number;
 		} catch (NumberFormatException e) {
 			System.out.println("An invalid number: '" + number + "' was entered. We set the number to 0000000000.");
-			this.number="0000000000";
+			this._number="0000000000";
 		}
 	}	
-	public String GetName(){return name;}
-	public String GetNumber(){return number;};
+	public String GetName(){return _name;}
+	public String GetNumber(){return _number;};
 	public Contact Get(){return this;};
-	public void PrintContact(){System.out.println(name + ": " + number);}
+	public void PrintContact(){System.out.println(_name + ": " + _number);}
 
 }
---
+++ Main_SMS.java
@@ -0,0 +1,7 @@
+
+public class Main_SMS {
+	public static void main(String[] args) {
+		Contact samy = new Contact("Samy", "0585896777");
+		PrivateChat chat1 = new PrivateChat(samy);
+	}
+}
